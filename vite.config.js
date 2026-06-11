ae1e43d079bd4025180738940446dead6338ed47
Jonathan Rodrigues <contatu.rodrigues@gmail.com>
2026-06-11 16:35:35 -0300

    Deploy transparent trophy cutout

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/copa-2026-simulador/',
-  plugins: [react(), tailwindcss()],
-})
